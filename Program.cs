f105311010772289143d61439e832f74472adc7b
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-08-16 10:51:10 +0300

    Задача №2

--- Program.cs
+++ Program.cs
@@ -1,18 +1,46 @@
 ﻿// Задача №1
+// Console.Write("Введите число №1: ");
+// int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число №2: ");
+// int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstnumber <= secondnumber)
+// {
+//      Console.WriteLine(firstnumber + " min " );
+//      Console.WriteLine(secondnumber + " max ");
+// }
+
+// else
+// {
+//       Console.WriteLine(firstnumber + " max " );
+//       Console.WriteLine(secondnumber + " min " );
+// }
+
+
+// Задача №2
 Console.Write("Введите число №1: ");
 int firstnumber = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите число №2: ");
 int secondnumber = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите число №3: ");
+int thirtnumber = Convert.ToInt32(Console.ReadLine());
+
 if (firstnumber <= secondnumber)
 {
-     Console.WriteLine(firstnumber + " min " );
-     Console.WriteLine(secondnumber + " max ");
+    if (secondnumber <= thirtnumber)
+        {
+            Console.WriteLine(thirtnumber + " max ");
+        }
+        else
+        {
+            Console.WriteLine(secondnumber + " max ");
+}
 }
 
 else
 {
-      Console.WriteLine(firstnumber + " max " );
-      Console.WriteLine(secondnumber + " min " );
+    Console.WriteLine(firstnumber + " max ");
 }
