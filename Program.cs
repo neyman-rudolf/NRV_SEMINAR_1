6809028966710ee2a6ef20ebf6ce9aa93180648d
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-08-16 11:06:04 +0300

    Задача №3

--- Program.cs
+++ Program.cs
@@ -19,28 +19,39 @@
 
 
 // Задача №2
-Console.Write("Введите число №1: ");
-int firstnumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число №1: ");
+// int firstnumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число №2: ");
-int secondnumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число №2: ");
+// int secondnumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число №3: ");
-int thirtnumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число №3: ");
+// int thirtnumber = Convert.ToInt32(Console.ReadLine());
 
-if (firstnumber <= secondnumber)
+// if (firstnumber <= secondnumber)
+// {
+//     if (secondnumber <= thirtnumber)
+//         {
+//             Console.WriteLine(thirtnumber + " max ");
+//         }
+//         else
+//         {
+//             Console.WriteLine(secondnumber + " max ");
+//         }
+// }
+
+// else
+// {
+//     Console.WriteLine(firstnumber + " max ");
+// }
+
+// Задача №3
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+
+while (N >= number)
 {
-    if (secondnumber <= thirtnumber)
-        {
-            Console.WriteLine(thirtnumber + " max ");
-        }
-        else
-        {
-            Console.WriteLine(secondnumber + " max ");
-        }
-}
-
-else
-{
-    Console.WriteLine(firstnumber + " max ");
+    Console.WriteLine(number);
+    number = number + 2;
 }
