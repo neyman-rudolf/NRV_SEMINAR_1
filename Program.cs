023449bc41fa6c3e0101ff89331414621ccef9ff
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-08-16 10:46:00 +0300

    Задача №1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача №1
+Console.Write("Введите число №1: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число №2: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstnumber <= secondnumber)
+{
+     Console.WriteLine(firstnumber + " min " );
+     Console.WriteLine(secondnumber + " max ");
+}
+
+else
+{
+      Console.WriteLine(firstnumber + " max " );
+      Console.WriteLine(secondnumber + " min " );
+}
